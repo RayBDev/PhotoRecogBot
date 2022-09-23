9d5892088b975138c4007292a855cc8d5efded40
RayBDev <raybernard1337@gmail.com>
2022-09-22 20:33:10 -0500

    Switched to manual cookie management

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Cookies from 'js-cookie';
+// import Cookies from 'js-cookie';
 import axios from 'axios';
 
 import Navigation from './components/Navigation/Navigation';
